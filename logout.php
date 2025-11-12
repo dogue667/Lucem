9382a0d2923052da5a2afbc824085a4f222b654f
dogue667 <guilhermefdocarmo12@gmail.com>
2025-11-12 15:34:24 -0300

    testes

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
- // remove todas as variáveis da sessão
-session_unset();
- // encerra a sessão
-session_destroy();
-// redireciona para a página publica
-header("Location: index.php");
-exit();
