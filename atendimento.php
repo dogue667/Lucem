f7c090bbb92ca3e6f29faf16a77e2c6d6f302e68
dogue667 <guilhermefdocarmo12@gmail.com>
2025-11-13 14:28:47 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- atendimento.php
+++ atendimento.php
@@ -200,28 +200,27 @@ session_start();
 <body>
 
   <!-- ---------- MENU SUPERIOR ---------- -->
-  <header>
-    <div class="logo">🌞 LUCEM</div>
-    <nav>
-      <ul>
-        <li><a href="index.php" style="font-weight:600; color:var(--roxo);">Sobre</a></li>
+<header>
+  <div class="logo">🌞 LUCEM</div>
+  <nav>
+    <ul>
+      <li><a href="index.php" style="font-weight:600; color:var(--roxo);">Sobre</a></li>
 
-        <?php if(!isset($_SESSION['usuario_id'])): ?>
-          <!-- VISÍVEL PARA VISITANTES -->
-          <li><a href="cadastro.html" style="color:#d9534f;">Criar Conta</a></li>
-          <li><a href="login.php" style="color:#d9534f;">Fazer login</a></li>
-        <?php else: ?>
-          <!-- VISÍVEL SOMENTE PARA LOGADOS -->
-          <li><a href="registra_emocoes.php">Registrar Emoções</a></li>
-          <li><a href="minhas_emocoes.php">Minhas Emoções</a></li>
-          <li><a href="atendimento.php" style="font-weight:600; color:var(--roxo);">Atendimento Psicológico</a></li>
-          <li><a href="artigos.php" style="font-weight:600; color:var(--roxo);">Artigos</a></li>
-          <li><a href="metas.php">Exercícios & Metas</a></li>
-          <li><a href="logout.php" style="color:#d9534f;">Sair</a></li>
-        <?php endif; ?>
-      </ul>
-    </nav>
-  </header>
+      <?php if(!isset($_SESSION['usuario_id'])): ?>
+        <!-- VISÍVEL PARA VISITANTES -->
+        <li><a href="cadastro.html"style="color:#d9534f;">Criar Conta</a></li>
+        <li><a href="login.php"style="color:#d9534f;">Fazer login</a></li>
+      <?php else: ?>
+        <!-- VISÍVEL SOMENTE PARA LOGADOS -->
+        <li><a href="registra_emocoes.php">Registrar Emoções</a></li>
+        <li><a href="atendimento.php" style="font-weight:600; color:var(--roxo);">Atendimento Psicológico</a></li>
+        <li><a href="artigos.php"style="font-weight:600; color:var(--roxo);">Artigos</a></li>
+        <li><a href="metas.php">Exercícios & Metas</a></li>
+        <li><a href="logout.php" style="color:#d9534f;">Sair</a></li>
+      <?php endif; ?>
+    </ul>
+  </nav>
+</header>
 
   <!-- ---------- CONTEÚDO PRINCIPAL ---------- -->
   <main>
